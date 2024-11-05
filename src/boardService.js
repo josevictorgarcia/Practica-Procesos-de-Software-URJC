6dc01da328086da6543e0b050211d8e58836bc58
Jordigb44 <jordigb44@gmail.com>
2024-11-05 20:24:34 +0100

    mejora en el dashboard en la comunicacion con los Manager y eliminacion de comunicacion con db

--- src/boardService.js
+++ src/boardService.js
@@ -1,4 +1,3 @@
-import DatabaseConnection from '../database/config/DatabaseConnection.js';
 import GameManager from '../database/managers/GameManager.js';
 import UserManager from '../database/managers/UserManager.js';
 
@@ -10,74 +9,48 @@ const user = {
 };
 */
 
-const posts = new Map([
-    [1, new Map()], /*Juegos de cartas */
-    [2, new Map()], /*Juegos de mesa */
-    [3, new Map()]  /*Máquinas tragaperras */
-]);
-let nextId = 0;
-
-/* Datos iniciales */
-addPost({nombre: "Blackjack", imagen: "https://images.sigma.world/blackjack-card-counting-scaled-1.jpg", url: "https://www.gamepix.com/play/las-vegas-blackjack", tipo: "Juegos de cartas"});
-addPost({nombre: "Parchis", imagen: "https://www.mora-play.com/mora/wp-content/uploads/2023/02/ref.-66-001-scaled.jpg", url: "https://www.ludoteka.com/juegos/parchis/parchis-individual-a-4", tipo: "Juegos de mesa"});
-addPost({nombre: "Ruleta", imagen: "https://www.rockaxis.com/img/newsList/8047148.png", url: "https://es.piliapp.com/random/wheel/", tipo: "Juegos de mesa"});
-addPost({nombre: "Juego Shooter", imagen: "https://images.crazygames.com/games/krunker-io/cover-1591336739727.png?auto=format,compress&q=75&cs=strip", url: "https://krunker.io", tipo: "Juegos de acción"});
-
-const dbConnection = new DatabaseConnection();
-
 export function isLogedIn(){
     if(!user){return (false, null)}
     return (true, user)
 }
 
-export function getUserData() {
-    return user;
-}
-
-export function addPost(post) {
-    let tipo;
-    if (post.tipo === "Juegos de cartas") {
-        tipo = 1;
-    } else if (post.tipo === "Juegos de mesa") {
-        tipo = 2; 
-    } else if (post.tipo === "Juegos de acción") {
-        tipo = 3;
+export async function getUserData(email) {
+    const userManager = new UserManager();
+    try {
+        const userInfo = await userManager.getUserInfo(email);
+        return userInfo; // Devuelve el userInfo
+    } catch (error) {
+        console.error(error);
+        throw error; // Opcional: vuelve a lanzar el error para manejarlo en otro lugar
     }
-    
-    let id = nextId++;
-    post.id = id.toString();
-    posts.get(tipo).set(post.id, post);
 }
 
 export async function getCartas() {
-    const gameManager = new GameManager(dbConnection);
-    await gameManager.initializeGames();
-    const games = await gameManager.getGamesByType('cartas');
-    console.log("Todos los juegos:", games);
-    return games
+    const gameManager = new GameManager();
+    return await gameManager.getAllGames("cartas");
 }
 
 export async function getMesa() {
-    const gameService = new GameManager(dbConnection);
-    return await gameService.getGamesByType('mesa'); // Asegúrate de que el tipo coincide con la base de datos
+    const gameManager = new GameManager();
+    return await gameManager.getAllGames("mesa");
 }
 
 export async function getAccion() {
-    const gameService = new GameManager(dbConnection);
-    return await gameService.getGamesByType('accion'); // Asegúrate de que el tipo coincide con la base de datos
+    const gameManager = new GameManager();
+    return await gameManager.getAllGames("accion");
 }
 
 export async function addUser(nombre, email, contraseña, profile_src) {
-    const userService = new UserManager(dbConnection);
+    const userService = new UserManager();
     return await userService.addUser(nombre, email, contraseña, profile_src);
 }
 
 export async function verifyUser(email, contraseña) {
-    const userService = new UserManager(dbConnection);
+    const userService = new UserManager();
     return await userService.verifyUser(email, contraseña);
 }
 
 export async function getUserInfo(email) {
-    const userService = new UserManager(dbConnection);
+    const userService = new UserManager();
     return await userService.getUserInfo(email);
 }
